4c9cda49b6fb00f23db207eb0ff3bc217aa34e4b
Joao73844 <joaorego41@gmail.com>
2017-04-02 21:47:16 +0100

    JR_Jitter Only some visual changes in the code

--- ProjectP3D/Header.h
+++ ProjectP3D/Header.h
@@ -1,5 +1,38 @@
-#include <stdlib.h>
-#include <GL/glut.h>
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <random>
 #include <stdio.h>
+#include <stdlib.h>
+// OpenGL
+#include <GL/glut.h>
+// Project Classes
 #include "scene.h"
+
+// CONSTANTS
+#define MAX_DEPTH 4
+#define EPSILON 1e-4
+#define IOR 1.0
+#define NUMEROAMOSTRAS 4
+#define ERAND (float)rand()/RAND_MAX
+// VARIABLES
+Scene * scene = NULL;
+int RES_X, RES_Y;
+Vect * rayTracing(Ray * ray, int depth, float ior);
+bool inShadow(Ray* ray);
+
+
+#define TIMING
+
+#ifdef TIMING
+#define INIT_TIMER auto start = std::chrono::high_resolution_clock::now();
+#define START_TIMER  start = std::chrono::high_resolution_clock::now();
+#define STOP_TIMER(name)  std::cout << "\n RUNTIME of " << name << ": " << \
+    std::chrono::duration_cast<std::chrono::milliseconds>( \
+            std::chrono::high_resolution_clock::now()-start \
+    ).count() << " ms " << std::endl; 
+#else
+#define INIT_TIMER
+#define START_TIMER
+#define STOP_TIMER(name)
+#endif
--- ProjectP3D/main.cpp
+++ ProjectP3D/main.cpp
@@ -1,34 +1,4 @@
 #include "Header.h"
-#include <vector>
-#include <random>
-
-#define MAX_DEPTH 4
-#define EPSILON 1e-4
-#define IOR 1.0
-#define NUMEROAMOSTRAS 4
-#define ERAND (float)rand()/RAND_MAX
-
-#include <chrono>
-
-#define TIMING
-
-#ifdef TIMING
-#define INIT_TIMER auto start = std::chrono::high_resolution_clock::now();
-#define START_TIMER  start = std::chrono::high_resolution_clock::now();
-#define STOP_TIMER(name)  std::cout << "\n RUNTIME of " << name << ": " << \
-    std::chrono::duration_cast<std::chrono::milliseconds>( \
-            std::chrono::high_resolution_clock::now()-start \
-    ).count() << " ms " << std::endl; 
-#else
-#define INIT_TIMER
-#define START_TIMER
-#define STOP_TIMER(name)
-#endif
-
-Scene * scene = NULL;
-int RES_X, RES_Y;
-Vect * rayTracing(Ray * ray, int depth, float ior);
-bool inShadow(Ray* ray);
 
 
 void reshape(int w, int h)
