121320254fd0c758d7af21e1fa21c9bb5598b8e4
paquiquinho <paquiquinho@gmail.com>
2017-04-11 15:21:52 +0100

    Grid setup with object lists per cell done;

--- ProjectP3D/Grid.cpp
+++ ProjectP3D/Grid.cpp
@@ -11,8 +11,8 @@ Grid::~Grid()
 {
 }
 
-void Grid::computeBBox(std::list<Obj*> objects)
-{
+void Grid::setup(std::list<Obj*> objects)
+{	//Compute grid's Bounding Box
 	BBox * bbox;
 	float min_point_X = HUGE_VALUE;
 	float min_point_Y = HUGE_VALUE;
@@ -45,8 +45,47 @@ void Grid::computeBBox(std::list<Obj*> objects)
 	Vect * min_point = new Vect(min_point_X - EPSILON, min_point_Y - EPSILON, min_point_Z - EPSILON);
 	Vect * max_point = new Vect(max_point_X + EPSILON, max_point_Y + EPSILON, max_point_Z + EPSILON);
 
-	this->_bbox = new BBox(min_point, max_point);
+	_bbox = new BBox(min_point, max_point);
 	
+	//Compute Nx,Ny,Nz
+	float wx = max_point->getX() - min_point->getX();
+	float wy = max_point->getY() - min_point->getY();
+	float wz = max_point->getZ() - min_point->getZ();
+
+	float s = cbrt((wx*wy*wz) / num_objects);
+	
+	_Nx = trunc(M*wx / s) + 1;
+	_Ny = trunc(M*wy / s) + 1;
+	_Nz = trunc(M*wz / s) + 1;
+
+	float cells_size = _Nx*_Ny*_Nz;
+
+	for (int i = 0; i < cells_size;i++) {
+		_cells->push_back(dynamic_cast<Cell*>(new Cell()));
+	}
+
+	for (std::list<Obj*>::iterator it = objects.begin(); it != objects.end(); it++) {
+		bbox = ((Obj*)*it)->get_bounding_box();
+		int ixmin = clamp((bbox->getMinX()-min_point->getX())*_Nx/(max_point->getX()-min_point->getX()),0,_Nx-1);
+		int iymin = clamp((bbox->getMinY() - min_point->getY())*_Ny / (max_point->getY() - min_point->getY()), 0, _Ny - 1);
+		int izmin = clamp((bbox->getMinZ() - min_point->getZ())*_Nz / (max_point->getZ() - min_point->getZ()), 0, _Nz - 1);
+		int ixmax = clamp((bbox->getMaxX() - min_point->getX())*_Nx / (max_point->getX() - min_point->getX()), 0, _Nx - 1);
+		int iymax = clamp((bbox->getMaxY() - min_point->getY())*_Ny / (max_point->getY() - min_point->getY()), 0, _Ny - 1);
+		int izmax = clamp((bbox->getMaxZ() - min_point->getZ())*_Nz / (max_point->getZ() - min_point->getZ()), 0, _Nz - 1);
+
+		for (int iz = izmin; iz <= izmax; iz++) {
+			for (int iy = iymin; iy <= iymax; iy++) {
+				for (int ix = ixmin; ix <= ixmax; ix++) {
+					float index = ix + _Nx*iy + _Nx*_Ny*iz;
+					_cells->at(index)->push((Obj*)*it);
+				}
+			}
+		}
+
+	}
 	delete min_point;
 	delete max_point;
+	delete bbox;
+
+	
 }
--- ProjectP3D/Grid.h
+++ ProjectP3D/Grid.h
@@ -1,22 +1,31 @@
 #pragma once
 // CPP files
 #include <list>
+#include<vector>
 // Project Files
 #include "BBox.h"
 #include "obj.h"
+#include "Cell.h"
 
 // Constants
 #define EPSILON 1e-4
 #define HUGE_VALUE 1000000
+#define M 2
 
 class Grid
 {
 	// This is a bounding box made from the bounding boxes
 	// of all the scene objects
 	BBox * _bbox;
+	float _Nx;
+	float _Ny;
+	float _Nz;
+	std::vector<Cell*>* _cells;
+
 public:
 	Grid();
 	~Grid();
-	void computeBBox(std::list<Obj*>);
+	void setup(std::list<Obj*>);
+	float clamp(float n, float min, float max) { return n <= min ? min : n >= max ? max : n; }
 };
 
